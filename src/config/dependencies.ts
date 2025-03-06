1aece6f14cb07f47d15ac502d6d274f6bafa915d
Kevin UNFRICHT <unfricht.kevin@hotmail.fr>
2025-03-06 22:41:34 +0100

    FIX (dependencies) production api

--- src/config/dependencies.ts
+++ src/config/dependencies.ts
@@ -1,7 +1,6 @@
 import { GameHistoryDataSource } from "../modules/game/datasource/game.datasource";
 import { inMemoryGameHistoryDatasource } from "../modules/game/datasource/inMemory.game.datasource";
 import { apiQuizzDatasource } from "../modules/quizz/datasource/api.quizz.datasource";
-import { fakeQuizzDatasource } from "../modules/quizz/datasource/fake.quizz.datasource";
 import { QuizzDataSource } from "../modules/quizz/datasource/quizz.datasource";
 
 export type Dependencies = {
@@ -10,6 +9,6 @@ export type Dependencies = {
 };
 
 export const dependencies: Dependencies = {
-  quizzDatasource: fakeQuizzDatasource,
+  quizzDatasource: apiQuizzDatasource,
   gameHistoryDatasource: inMemoryGameHistoryDatasource,
 };
