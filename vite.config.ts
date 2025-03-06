b736ce7e460e98f48338feab4867b58177a58950
Kevin UNFRICHT <unfricht.kevin@hotmail.fr>
2025-03-06 22:39:36 +0100

    FIX (vite) config to base folder github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // URL de base de l'application, qui est situé dans un sous-dossier
+  // Dans le cas d'un déploiement via github pages quand l'url est sous la forme de https://<USERNAME>.github.io/<REPO>/
+  //
+  // Voir https://vite.dev/guide/static-deploy#github-pages
+  base: "/School_MiniProjet_Kouizz_React/",
+});
